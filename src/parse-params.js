29ddbf5ebd43719ec6888e37e758dae309d5202f
UpperCod <mtrujillo@outlook.cl>
2021-10-19 11:44:08 -0300

    prepare

---
+++ src/parse-params.js
@@ -0,0 +1,66 @@
+/**
+ * captures the parameters of a string
+ * @example
+ * ```js
+ * parseParams(`var(--z)`) // [ [["var","--z"]] ]
+ * ```
+ * @param {string} input
+ * @returns {[string,string[]][]}
+ */
+export function parseParams(input) {
+    input = input.replace(/\s+/g, " ") + " ";
+
+    let group = [];
+    const groups = [group];
+    let count = 0;
+    let position = 0;
+    let parentWord = "";
+    let word = "";
+    let params;
+    let lastQuote;
+
+    const addParams = (param) => params && params.push(param.trim());
+
+    const addUtil = (word, params) => group.push([word, params]);
+
+    while (position < input.length) {
+        const letter = input[position++];
+        if (count && (letter == '"' || letter == "'")) {
+            if (lastQuote === letter) {
+                lastQuote = "";
+                continue;
+            } else {
+                if (!lastQuote) {
+                    lastQuote = letter;
+                    continue;
+                }
+            }
+        }
+        if (lastQuote) {
+            word += letter;
+        } else if (letter == "," && !count) {
+            groups.push((group = []));
+        } else if (letter == "," && count == 1) {
+            addParams(word);
+            word = "";
+        } else if (letter == " " && !count && (parentWord || word)) {
+            addUtil(parentWord || word, []);
+            word = "";
+            continue;
+        } else if (letter == "(" && !count++) {
+            parentWord = word;
+            params = [];
+            word = "";
+            continue;
+        } else if (letter == ")" && !--count) {
+            addParams(word);
+            addUtil(parentWord, params);
+            parentWord = word = "";
+            continue;
+        } else {
+            word += letter == " " && !count ? "" : letter;
+        }
+    }
+
+    return groups;
+}
