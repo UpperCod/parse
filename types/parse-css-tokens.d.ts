57103d2fcd7215617f9ab7b8ee138ba0bf79d741
UpperCod <mtrujillo@outlook.cl>
2021-10-25 11:52:32 -0300

    0.2.0

---
+++ types/parse-css-tokens.d.ts
@@ -0,0 +1,22 @@
+export function createParseCssTokens({ prefix, tokens }: {
+    prefix: string;
+    tokens: {
+        [x: string]: Tokens;
+    };
+}): {
+    /**
+     * @type {Object<string,Tokens>}
+     */
+    tokens: {
+        [x: string]: Tokens;
+    };
+    /**
+     * @param {TemplateStringsArray}
+     */
+    tokenize({ raw }: TemplateStringsArray): string;
+};
+export type Tokens = {
+    title: string;
+    type?: string | undefined;
+    children: [string, string][];
+};
