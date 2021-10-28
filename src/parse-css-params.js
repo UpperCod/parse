5598accc97890df7639d9ce511f418084ef4e96a
UpperCod <mtrujillo@outlook.cl>
2021-10-28 01:52:11 -0300

    fix parse-css-params

--- src/parse-css-params.js
+++ src/parse-css-params.js
@@ -22,7 +22,9 @@ export function parseCssParams(input) {
     const addParams = (param) =>
         params && params.push(param.trim() || undefined);
 
-    const addUtil = (word, params) => group.push([word, params]);
+    const addUtil = (word, params) => {
+        group.push([word, params]);
+    };
 
     while (position < input.length) {
         const letter = input[position++];
@@ -37,15 +39,20 @@ export function parseCssParams(input) {
                 }
             }
         }
+        let currentWord = parentWord || word;
         if (lastQuote) {
             word += letter;
         } else if (letter == "," && !count) {
+            if (currentWord) {
+                addUtil(currentWord, []);
+                word = "";
+            }
             groups.push((group = []));
         } else if (letter == "," && count == 1) {
             addParams(word);
             word = "";
-        } else if (letter == " " && !count && (parentWord || word)) {
-            addUtil(parentWord || word, []);
+        } else if (letter == " " && !count && currentWord) {
+            addUtil(currentWord, []);
             word = "";
             continue;
         } else if (letter == "(" && !count++) {
